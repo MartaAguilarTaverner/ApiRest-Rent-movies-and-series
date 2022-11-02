4817c2767b955d25dd236fa8e38020d6b0bfae38
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2022-11-02 21:26:51 +0100

    Basic structure of back and front

---
+++ Backside/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const router = require("./router");
+const PORT = 3030;
+app.use(express.json());
+
+app.use(router);
+ç;
+app.listen(PORT, () => {});
